b6ef3a7db98c08ac00d3e8304c8796c758f9719f
Juliana <ju.s.oliveira23@gmail.com>
2025-11-18 23:13:39 -0300

    Atualizações

--- AutoCollections/AutoCollections/Models/UsuNivel.cs
+++ AutoCollections/AutoCollections/Models/UsuNivel.cs
@@ -1,6 +1,13 @@
-﻿namespace AutoCollections.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoCollections.Models
 {
     public class UsuNivel
     {
+        [Display(Description = "Id do Usuario")]
+        public int IdUsuario { get; set; }
+
+        [Display(Description = "Nome do Nivel")]
+        public int IdNivel { get; set; }
     }
 }
