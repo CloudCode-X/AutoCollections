49080ac3ece1d609b1c75b4b3c899133c979dfe7
Juliana <ju.s.oliveira23@gmail.com>
2025-12-02 17:08:23 -0300

    Criando a model ProdutoCatalogoViewModel

---
+++ AutoCollections/Models/ProdutoCatalogoViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AutoCollections.Models
+{
+    public class ProdutoCatalogoViewModel
+    {
+        public string Categoria { get; set; }
+        public List<ItemCatalogoViewModel> Produtos { get; set; }
+    }
+
+    public class ItemCatalogoViewModel
+    {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public string ImagemURL { get; set; }
+    }
+}
