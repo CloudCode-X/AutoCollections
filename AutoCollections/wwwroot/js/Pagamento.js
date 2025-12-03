ed06bd7e11c9f1aa4abae4ef8f11c80a2bc5f6d9
Juliana <ju.s.oliveira23@gmail.com>
2025-12-03 03:25:03 -0300

    Criando o pagamento

---
+++ AutoCollections/wwwroot/js/Pagamento.js
@@ -0,0 +1,10 @@
+﻿document.getElementById('payButton').addEventListener('click', function() {
+    document.getElementById('successMessage').style.display = 'block';
+
+    if (window.opener) {
+        window.opener.adicionarCompraEmAndamento?.();
+    }
+
+    this.disabled = true;
+    this.textContent = 'Pagamento concluído';
+});
